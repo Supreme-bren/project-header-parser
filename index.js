963f4e1144e0b48b5dc626bf759fc560e4b5b5df
Brennan <brennanosunkwo14@gmail.com>
2023-12-29 21:04:45 -0500

    completed header-parser project

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami',(req, res) => {
+  let ipAddress = req.ip;
+  let language = req.get("Accept-language");
+  let software = req.get("user-agent")
+  res.json({
+    ipaddress: `${ipAddress}`,
+    language: `${language}`,
+    software: `${software}`
+  })
 });
 
 // listen for requests :)
